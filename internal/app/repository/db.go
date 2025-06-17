acc623e5ecc9ef3e02df1bc0d40ccfc7a83a8d33
lzh <1625167628@qq.com>
2025-06-17 11:02:15 +0800

    1

--- internal/app/repository/db.go
+++ internal/app/repository/db.go
@@ -41,7 +41,7 @@ func InitDb() {
 	}
 	sqlDB.SetConnMaxLifetime(time.Hour)
 	db = gdb.Session(&defaultConfig)
-	db = gdb.Session(&defaultConfig).Debug()
+	// db = gdb.Session(&defaultConfig).Debug()
 	db.AutoMigrate(&model.Process{}, &model.User{}, &model.Permission{}, &model.Push{}, &model.Config{}, &model.ProcessLog{}, &model.Task{}, &model.WsShare{})
 
 	// g := gen.NewGenerator(gen.Config{
