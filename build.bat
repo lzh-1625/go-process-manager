b4c7ab9f2c58802ea449007acd4b5b3949b852d2
lzh <1625167628@qq.com>
2025-07-11 15:42:00 +0800

    add build params

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-@REM go build -ldflags="-s -w" -tags="slim" -o go_process_manager cmd/go_process_manager/main.go
-go build -ldflags="-s -w" -o go_process_manager cmd/go_process_manager/main.go
+@REM go build -ldflags="-s -w" -tags="slim" -o go_process_manager -trimpath cmd/go_process_manager/main.go
+go build -ldflags="-s -w" -o go_process_manager -trimpath cmd/go_process_manager/main.go
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export CGO_ENABLED=0
 export GOOS=linux
 export GOARCH=amd64
-# go build -ldflags="-s -w" -tags="slim" -o go_process_manager cmd/go_process_manager/main.go
-go build -ldflags="-s -w" -o go_process_manager cmd/go_process_manager/main.go
+# go build -ldflags="-s -w" -tags="slim" -o go_process_manager -trimpath cmd/go_process_manager/main.go
+go build -ldflags="-s -w" -o go_process_manager -trimpath cmd/go_process_manager/main.go
