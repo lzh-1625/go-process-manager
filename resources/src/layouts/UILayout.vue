5de8b45b2f1f959cbae26a150bdec2ef05ef3a2f
akrike <1625167628@qq.com>
2025-08-13 21:39:13 +0800

    init web ui

---
+++ resources/src/layouts/UILayout.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import Breadcrumb from "@/components/Breadcrumb.vue";
+import PageTitle from "@/components/PageTitle.vue";
+import MainSidevar from "@/components/navigation/MainSidebar.vue";
+import MainAppbar from "@/components/toolbar/MainAppbar.vue";
+import ToolBox from "@/components/Toolbox.vue";
+import { useCustomizeThemeStore } from "@/stores/customizeTheme";
+const customizeTheme = useCustomizeThemeStore();
+
+// import GlobalLoading from "@/components/GlobalLoading.vue";
+</script>
+
+<template>
+  <!-- ---------------------------------------------- -->
+  <!---Main Sidebar -->
+  <!-- ---------------------------------------------- -->
+  <MainSidevar />
+  <!-- ---------------------------------------------- -->
+  <!---Top AppBar -->
+  <!-- ---------------------------------------------- -->
+  <MainAppbar />
+  <!-- ---------------------------------------------- -->
+  <!---MainArea -->
+  <!-- ---------------------------------------------- -->
+  <v-main
+    v-touch="{
+      left: () => (customizeTheme.mainSidebar = false),
+      right: () => (customizeTheme.mainSidebar = true),
+    }"
+    class="main-container"
+  >
+    <!-- <GlobalLoading /> -->
+
+    <v-card class="d-none d-sm-block px-5" rounded="0" color="primary">
+      <PageTitle></PageTitle>
+      <Breadcrumb></Breadcrumb>
+    </v-card>
+
+    <div class="flex-fill">
+      <slot></slot>
+    </div>
+    <ToolBox />
+  </v-main>
+</template>
+
+<style scoped>
+.scrollnav {
+  height: calc(100vh - 326px);
+}
+
+.main-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
