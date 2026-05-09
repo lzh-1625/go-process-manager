bb9facac4b11955c87ba5a3d31b67189d74945a9
刘志杭 <zhihang.liu@xiaoxitech.com>
2026-05-09 17:27:45 +0800

    Improve ProcessState documentation in process.go for clarity; update task execution logic to check for both running and starting states in task stop conditions

--- internal/app/eum/process.go
+++ internal/app/eum/process.go
@@ -3,8 +3,8 @@ package eum
 type ProcessState int32
 
 const (
-	ProcessStateStop ProcessState = iota
-	ProcessStateStart
-	ProcessStateWarnning
-	ProcessStateRunning
+	ProcessStateStop     ProcessState = iota // process is stopped
+	ProcessStateStart                        // process is starting
+	ProcessStateWarnning                     // process is in warning state
+	ProcessStateRunning                      // process is running
 )
