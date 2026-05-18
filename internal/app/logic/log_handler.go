0242d2964ebf47e253a34c8c8413810c8495b858
akrike <1625167628@qq.com>
2026-05-18 10:53:07 +0800

    Replace ants pool with diskqueue for log handling in LogHandler

--- internal/app/logic/log_handler.go
+++ internal/app/logic/log_handler.go
@@ -1,54 +1,73 @@
 package logic
 
 import (
+	"encoding/json"
+	"os"
+	"path"
+	"sync"
 	"time"
 
 	"github.com/lzh-1625/go_process_manager/config"
 	"github.com/lzh-1625/go_process_manager/internal/app/model"
 	"github.com/lzh-1625/go_process_manager/internal/app/repository/search"
 	"github.com/lzh-1625/go_process_manager/log"
+	"github.com/lzh-1625/go_process_manager/utils"
 
-	"github.com/panjf2000/ants/v2"
+	"github.com/nsqio/go-diskqueue"
 )
 
 type LogHandler struct {
-	antsPool  *ants.Pool
+	queue     diskqueue.Interface
 	ILogLogic search.ILogLogic
+	wg        *sync.WaitGroup
 }
 
-func NewLogHandler(ILogLogic search.ILogLogic) *LogHandler {
-	options := []ants.Option{}
-	options = append(options, ants.WithExpiryDuration(time.Minute*10))
-	options = append(options, ants.WithPanicHandler(func(i any) {
-		log.Logger.Warnw("log storage failed", "err", i)
-	}))
-	if config.CF.LogHandlerMaxBlockingTasks > 0 {
-		options = append(options, ants.WithMaxBlockingTasks(config.CF.LogHandlerMaxBlockingTasks))
-	} else {
-		options = append(options, ants.WithNonblocking(true))
+func NewLogHandler(logLogic search.ILogLogic) *LogHandler {
+	dirPath := path.Join(utils.UnwarpIgnore(os.UserHomeDir()), ".gpm", "log.diskqueue")
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		log.Logger.Panic(err)
 	}
-	antsPool, _ := ants.NewPool(config.CF.LogHandlerPoolSize, options...)
-	return &LogHandler{
-		antsPool:  antsPool,
-		ILogLogic: ILogLogic,
+	queue := diskqueue.New(
+		"log",
+		dirPath,
+		1024*1024*512*1,
+		4,
+		1024*1024*10,
+		1000,
+		5*time.Second,
+		func(lvl diskqueue.LogLevel, f string, args ...any) {
+			switch lvl {
+			case diskqueue.DEBUG, diskqueue.INFO:
+				log.Logger.Debugf(f, args...)
+			case diskqueue.WARN:
+				log.Logger.Warnf(f, args...)
+			case diskqueue.ERROR:
+				log.Logger.Errorf(f, args...)
+			}
+		})
+
+	wg := &sync.WaitGroup{}
+	for range max(1, config.CF.LogHandlerPoolSize) {
+		wg.Go(func() {
+			var pl model.ProcessLog
+			for msg := range queue.ReadChan() {
+				_ = json.Unmarshal(msg, &pl)
+				logLogic.Insert(pl.Log, pl.Name, pl.Using, pl.Time)
+			}
+		})
 	}
+	return &LogHandler{queue: queue, ILogLogic: logLogic, wg: wg}
 }
 
 func (l *LogHandler) AddLog(data model.ProcessLog) {
-	if err := l.antsPool.Submit(func() {
-		l.ILogLogic.Insert(data.Log, data.Name, data.Using, data.Time)
-	}); err != nil {
-		log.Logger.Warnw("coroutine pool add task failed", "err", err, "current running number", l.antsPool.Running())
-	}
+	l.queue.Put(utils.Unwarp(json.Marshal(data)))
 }
 
 func (l *LogHandler) GetRunning() int {
-	return l.antsPool.Running()
+	return int(l.queue.Depth())
 }
 
 func (l *LogHandler) Close() {
-	err := l.antsPool.ReleaseTimeout(time.Second * 10)
-	if err != nil {
-		log.Logger.Warnw("log handler close failed", "err", err)
-	}
+	l.queue.Close()
+	l.wg.Wait()
 }
