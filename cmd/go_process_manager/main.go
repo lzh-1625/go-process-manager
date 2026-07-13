09377fd125a682152ca44628cde288cfe1b0f8d2
lzh <10733914+akrike@user.noreply.gitee.com>
2026-07-13 14:09:27 +0800

    refactor: use manual dependency injection

--- cmd/go_process_manager/main.go
+++ cmd/go_process_manager/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"context"
+	"errors"
+	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"github.com/labstack/echo/v5"
 	"github.com/lzh-1625/go_process_manager/config"
-	"github.com/lzh-1625/go_process_manager/internal/app"
+	"github.com/lzh-1625/go_process_manager/internal/app/api"
 	"github.com/lzh-1625/go_process_manager/internal/app/logic"
+	"github.com/lzh-1625/go_process_manager/internal/app/middle"
+	"github.com/lzh-1625/go_process_manager/internal/app/repository"
+	"github.com/lzh-1625/go_process_manager/internal/app/route"
 	"github.com/lzh-1625/go_process_manager/log"
 	"github.com/robfig/cron/v3"
 	"github.com/spf13/cobra"
-	"go.uber.org/fx"
 )
 
 func main() {
@@ -82,59 +87,97 @@ var runCmd = &cobra.Command{
 	Short: "Run gpm in the foreground",
 	Long:  `Run gpm in the foreground.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fx.New(
-			fx.NopLogger,
-			fx.StopTimeout(time.Second*5+time.Duration(config.CF.KillWaitTime)*time.Second),
-			app.Module,
-			fx.Invoke(func(
-				r *echo.Echo,
-				lc fx.Lifecycle,
-				processCtlLogic *logic.ProcessCtlLogic,
-				taskLogic *logic.TaskLogic,
-				logHandler *logic.LogHandler,
-				eventLogic *logic.EventLogic,
-				eventBus *logic.EventBus,
-			) {
-				c := cron.New()
-				lc.Append(fx.Hook{
-					OnStart: func(ctx context.Context) error {
-						print(startTitle)
-						go func() {
-							log.Logger.Infow("starting echo server", "listen", config.CF.Listen)
-							err := r.Start(config.CF.Listen)
-							if err != nil {
-								log.Logger.Panicw("start echo server failed", "err", err)
-							}
-						}()
-						// event cleaning cron job
-						if config.CF.EventStorageTime >= 0 {
-							c.AddFunc("0 3 * * *", func() {
-								log.Logger.Infow("event cleaning execution")
-								eventLogic.Clean(time.Duration(config.CF.EventStorageTime) * time.Hour * 24)
-							})
-							c.Start()
-						}
-						processCtlLogic.ProcessInit()
-						taskLogic.InitTaskJob()
-						go func() {
-							// run task by trigger event
-							for event := range eventBus.Subscribe() {
-								go taskLogic.RunTaskByTriggerEvent(event.Proc.Name, event.State)
-							}
-						}()
-						return nil
-					},
-					OnStop: func(ctx context.Context) error {
-						c.Stop()
-						log.Logger.Infow("waiting for all process to stop")
-						processCtlLogic.KillAllProcess()
-						logHandler.Close()
-						eventBus.Close()
-						print(stopTitle)
-						return nil
-					},
-				})
-			}),
-		).Run()
+		run()
 	},
+}
+
+func run() {
+	db := repository.NewDB()
+	query := repository.NewQuery(db)
+	processRepository := repository.NewProcessRepository(query)
+	userRepository := repository.NewUserRepository(query)
+	pushRepository := repository.NewPushRepository(query)
+	eventRepository := repository.NewEventRepository(query)
+	permissionRepository := repository.NewPermissionRepository(query)
+	logRepository := repository.NewLogRepository(query)
+	wsShareRepository := repository.NewWsShareRepository(query)
+	taskRepository := repository.NewTaskRepository(query)
+
+	logLogic := logic.NewILogLogic(logRepository)
+	eventBus := logic.NewEventBus()
+	logHandler := logic.NewLogHandler(logLogic)
+	eventLogic := logic.NewEventLogic(eventRepository)
+	permissionLogic := logic.NewPermissionLogic(permissionRepository)
+	pushLogic := logic.NewPushLogic(pushRepository)
+	processCtlLogic := logic.NewProcessCtlLogic(processRepository, permissionRepository, eventLogic, pushLogic, logHandler, eventBus)
+	taskLogic := logic.NewTaskLogic(taskRepository, eventLogic, processCtlLogic, eventBus)
+	userLogic := logic.NewUserLogic(userRepository)
+	wsShareLogic := logic.NewWsShareLogic(wsShareRepository)
+	configLogic := logic.NewConfigLogic()
+	metricLogic := logic.NewMetricLogic(processCtlLogic, logHandler, logLogic)
+
+	permissionAPI := api.NewPermissionApi(permissionLogic)
+	wsAPI := api.NewWsApi(processCtlLogic, wsShareLogic, eventLogic, permissionAPI)
+	procAPI := api.NewProcApi(processCtlLogic, wsShareLogic, permissionAPI)
+	taskAPI := api.NewTaskApi(taskLogic)
+	userAPI := api.NewUserApi(userLogic)
+	pushAPI := api.NewPushApi(pushLogic)
+	eventAPI := api.NewEventApi(eventLogic)
+	logAPI := api.NewLogApi(permissionLogic, logLogic)
+	configAPI := api.NewConfigApi(configLogic, logLogic)
+	metricAPI := api.NewMetricApi(metricLogic)
+	loggerMiddleware := middle.NewEventLoggerMiddleware(eventLogic)
+	authMiddleware := middle.NewAuthMiddleware(userLogic)
+	r := route.NewRoute(wsAPI, procAPI, taskAPI, userAPI, pushAPI, eventAPI, permissionAPI, logAPI, configAPI, metricAPI, loggerMiddleware, authMiddleware)
+	server := &http.Server{
+		Addr:    config.CF.Listen,
+		Handler: r,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	serverErr := make(chan error, 1)
+	print(startTitle)
+	go func() {
+		log.Logger.Infow("starting echo server", "listen", config.CF.Listen)
+		serverErr <- server.ListenAndServe()
+	}()
+
+	c := cron.New()
+	// event cleaning cron job
+	if config.CF.EventStorageTime >= 0 {
+		c.AddFunc("0 3 * * *", func() {
+			log.Logger.Infow("event cleaning execution")
+			eventLogic.Clean(time.Duration(config.CF.EventStorageTime) * time.Hour * 24)
+		})
+		c.Start()
+	}
+	processCtlLogic.ProcessInit()
+	taskLogic.InitTaskJob()
+	go func() {
+		// run task by trigger event
+		for event := range eventBus.Subscribe() {
+			go taskLogic.RunTaskByTriggerEvent(event.Proc.Name, event.State)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+	case err := <-serverErr:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Logger.Errorw("echo server stopped", "err", err)
+		}
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second+time.Duration(config.CF.KillWaitTime)*time.Second)
+	defer cancel()
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Logger.Errorw("shutdown echo server failed", "err", err)
+	}
+	c.Stop()
+	log.Logger.Infow("waiting for all process to stop")
+	processCtlLogic.KillAllProcess()
+	logHandler.Close()
+	eventBus.Close()
+	print(stopTitle)
 }
